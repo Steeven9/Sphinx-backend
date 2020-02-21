f3be25d1534c59fade9a9a974270a1eabed643d7
Stefano Taillefert <taills@usi.ch>
2020-02-21 14:24:31 +0100

    added Spring template

---
+++ src/test/java/ch/usi/inf/sa4/sphinx/SphinxApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.usi.inf.sa4.sphinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SphinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
