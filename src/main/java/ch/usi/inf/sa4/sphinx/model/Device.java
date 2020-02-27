492eaded6e586148110b83c7a8e8d546059edaf2
Stefano Taillefert <taills@usi.ch>
2020-02-27 15:14:47 +0100

    fixed access and added IDs

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Device.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Device.java
@@ -3,15 +3,20 @@ package ch.usi.inf.sa4.sphinx.model;
 import java.util.ArrayList;
 
 public abstract class Device {
-    protected String icon;
-    protected String name;
-    protected ArrayList<SwitchInterface> ors;
-    protected ArrayList<SwitchInterface> xors;
-    protected boolean on;
+    public int id;
+    public String icon;
+    public String name;
+    public ArrayList<SwitchInterface> ors;
+    public ArrayList<SwitchInterface> xors;
+    public boolean on;
+    private static int nextId = 0;
+
+    private static int makeId() { return nextId++; }
 
     public abstract String getLabel();
 
     public Device() {
+        id = makeId();
         icon = "/images/generic_device";
         name = "Device";
         ors = new ArrayList<>();
