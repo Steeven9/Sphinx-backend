7d6f21ce47a431981886db9b3bbf1a2fb8ea3cf2
terehm <terehm@usi.ch>
2020-02-27 07:48:16 +0100

    Create abstract Device class

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Device.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Device.java
@@ -1,5 +1,22 @@
 package ch.usi.inf.sa4.sphinx.model;
 
+import java.util.ArrayList;
+
 public abstract class Device {
+    protected String icon;
+    protected String name;
+    protected List<SwitchInterface> ors;
+    protected List<SwitchInterface> xors;
+    protected boolean on;
+
+    public abstract String getLabel();
+
+    public Device() {
+        icon = "/images/generic_device";
+        name = "Device";
+        ors = new ArrayList<SwitchInterface>();
+        xors = new ArrayList<SwitchInterface>();
+        on = true;
+    }
 
 }
