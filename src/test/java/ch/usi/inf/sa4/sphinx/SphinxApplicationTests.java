244ffd2a091c92e29eafd44ba08a236284667c5b
terehm <terehm@usi.ch>
2020-02-28 10:29:32 +0100

    add some tests

--- src/test/java/ch/usi/inf/sa4/sphinx/SphinxApplicationTests.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/SphinxApplicationTests.java
@@ -1,13 +1,21 @@
 package ch.usi.inf.sa4.sphinx;
 
+import ch.usi.inf.sa4.sphinx.controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SphinxApplicationTests {
 
+	@Autowired
+	private UserController userController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(userController).isNotNull();
 	}
 
 }
