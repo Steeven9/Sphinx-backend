c882fe3a7e8b986071931962d81b95599009e776
canzom <canzoniere70@gmail.com>
2020-02-28 10:12:36 +0100

    SwitchInterface, Switch and DimmableSwitch modified (done)

--- src/main/java/ch/usi/inf/sa4/sphinx/model/DimmableSwitch.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/DimmableSwitch.java
@@ -1,5 +1,13 @@
 package ch.usi.inf.sa4.sphinx.model;
 
-public abstract class DimmableSwitch extends Device implements SwitchInterface {
+public  class DimmableSwitch extends Device implements SwitchInterface {
+    private int state;
+    public int getPower(){
+        return state;
+    }
+    public String getLabel(){
+     return state + "%";
+    }
+
 
 }
