1ac1853968e10bcb7c5b0daba0c7c89d58bd8ab6
terehm <terehm@usi.ch>
2020-03-02 15:50:54 +0100

    implement login and verify

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/AuthController.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/AuthController.java
@@ -1,10 +1,54 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ch.usi.inf.sa4.sphinx.model.User;
+import ch.usi.inf.sa4.sphinx.view.SerialisableUser;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody SerialisableUser givenUser) {
+        if (givenUser.email == null && givenUser.username == null) {
+            return ResponseEntity.status(400).build();
+        }
+
+        User user;
+        if (givenUser.username != null) {
+            user = Storage.getUser(givenUser.username);
+        } else {
+            user = Storage.getUserByEmail(givenUser.email);
+        }
+
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (!user.verified) {
+            return ResponseEntity.status(403).build();
+        }
+        if (user.password != givenUser.password) {
+            return ResponseEntity.status(401).build();
+        }
+
+        return ResponseEntity.ok(user.createSessionToken()).build();
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<SerialisableUser> verifyUser(@RequestBody User fakeUser) {
+        User verifiedUser = Storage.getUser(fakeUser.username);
+        if (verifiedUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+        if (verifiedUser.verified) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (verifiedUser.verificationToken != fakeUser.verificationToken) {
+            return ResponseEntity.status(401).build();
+        }
+        verifiedUser.verified = true;
+        return ResponseEntity.ok().build();
+    }
 
 }
