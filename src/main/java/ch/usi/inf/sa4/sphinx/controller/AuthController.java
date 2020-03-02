5ef9456d083affa5f055a88e293bdc9dfe43cdc3
terehm <terehm@usi.ch>
2020-03-02 14:27:33 +0100

    create initial AuthController file

---
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/AuthController.java
@@ -0,0 +1,10 @@
+package ch.usi.inf.sa4.sphinx.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+}
