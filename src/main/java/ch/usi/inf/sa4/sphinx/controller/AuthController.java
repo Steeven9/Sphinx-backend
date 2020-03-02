df4299e3bfc11051409096aea29b88f5603bbab5
terehm <terehm@usi.ch>
2020-03-02 16:40:30 +0100

    add javadoc on part of the AuthController

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/AuthController.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/AuthController.java
@@ -9,6 +9,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
+    /**
+     * Logs in using given credentials.
+     * @param givenUser an empty User containing the credentials provided by the client, parsed from json.
+     * @return A ResponseEntity containing status code 200 and the new session token as a body if successful or
+     *      status 400 if no username or email is provided
+     *      status 404 if provided username or email do not correspond to any user
+     *      status 403 if the user is not verified yet
+     *      status 401 if the password was incorrect
+     */
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody SerialisableUser givenUser) {
         if (givenUser.email == null && givenUser.username == null) {
