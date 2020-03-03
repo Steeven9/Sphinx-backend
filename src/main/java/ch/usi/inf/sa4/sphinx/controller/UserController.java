af66e85fdfdd0322d6593fe4dd3ed0319426e5bb
terehm <terehm@usi.ch>
2020-03-03 07:47:10 +0100

    add documentation to UserController

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/UserController.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/UserController.java
@@ -17,6 +17,14 @@ import java.util.Objects;
 @RequestMapping("/user")
 public class UserController {
 
+	/**
+	 * gets a user.
+	 * @param username the username of the requested user
+	 * @param session_token the session token used for authentication
+	 * @return a ResponseEntity with the data of the requested user if successful or
+	 * 		status code 404 if no user with the requested username exists
+	 * 		status code 401 if the provided session token does not match (or does not exist)
+	 */
 	@GetMapping("/{username}")
 	public ResponseEntity<SerialisableUser> getUser(@PathVariable String username, @RequestHeader("session-token") String session_token) {
 
@@ -25,12 +33,19 @@ public class UserController {
 		if (user == null) {
 			return ResponseEntity.notFound().build();
 		}
-		if (session_token != user.session_token) {
+		if (session_token == null || session_token != user.session_token) {
 			return ResponseEntity.status(401).build();
 		}
 		return ResponseEntity.ok(new SerialisableUser(user));
 	}
 
+	/**
+	 * creates a new user.
+	 * @param username the username of the user to create
+	 * @param user a SerialisableUser with the data of the user to create
+	 * @return a ResponseEntity with status code 203 and a body with the newly-created user's data if the process was successful or
+	 * 		400 if some data was missing or the usernames do not match
+	 */
 	@PostMapping("/{username}")
 	public ResponseEntity<SerialisableUser> createUser(@PathVariable String username, @RequestBody SerialisableUser user) {
 		if (user.email == null || user.fullname == null || user.password == null || !Objects.equals(user.username, username)) {
@@ -43,6 +58,15 @@ public class UserController {
 		return ResponseEntity.status(203).body(new SerialisableUser(newUser));
 	}
 
+	/**
+	 * changes a user's information
+	 * @param username the username of the user to change
+	 * @param user a SerialisableUser containing the new data of the user
+	 * @param session_token the session token used for authentication
+	 * @return a ResponseEntity with status 200 and body containing the data of the changed user or
+	 * 		404 if no user with the requested username exists
+	 * 		403 if the provided session token does not match the requested user (or none was provided)
+	 */
 	@PutMapping("/{username}")
 	public ResponseEntity<SerialisableUser> updateUser(@PathVariable String username, @RequestBody SerialisableUser user,
 													@RequestHeader("session-token") String session_token) {
@@ -61,6 +85,15 @@ public class UserController {
 		return ResponseEntity.ok(new SerialisableUser(changedUser));
 	}
 
+	/**
+	 * deletes a user
+	 * @param username the username of the user to delete
+	 * @param session_token the session token used to authenticate
+	 * @return a ResponseEntity containing one of the following status codes:
+	 * 		404 if no user with the given username exists
+	 * 		403 if the session token does not match
+	 * 		204 if the operation was successful
+	 */
 	@DeleteMapping("/{username}")
 	public ResponseEntity<SerialisableUser> deleteUser(@PathVariable String username,
 													   @RequestHeader("session-token") String session_token) {
