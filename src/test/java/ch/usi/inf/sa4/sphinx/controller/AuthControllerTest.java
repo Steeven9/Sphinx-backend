418124e576c6572d7e82ee6e9b3448720cebbeba
terehm <terehm@usi.ch>
2020-03-03 12:47:36 +0100

    start writing tests for AuthController

---
+++ src/test/java/ch/usi/inf/sa4/sphinx/controller/AuthControllerTest.java
@@ -0,0 +1,46 @@
+package ch.usi.inf.sa4.sphinx.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockmvc;
+
+    @Test
+    public void shouldGet400OnLoginWithNoData() throws Exception {
+        this.mockmvc.perform(get("/auth/login"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldGet404OnLoginWithWrongUsername() throws Exception {
+        this.mockmvc.perform(get("/auth/login").content("{username:\"test\"}"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldGet404OnLoginWithWrongEmail() throws Exception {
+        this.mockmvc.perform(get("/auth/login").content("{username:\"test@email.io\"}"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+
+}
