6c10a553cfc67ab98f917c01ed9684ad52133aaf
terehm <terehm@usi.ch>
2020-03-04 12:54:25 +0100

    create insert/delete/get methods for rooms and devices

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/Storage.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/Storage.java
@@ -1,5 +1,7 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
+import ch.usi.inf.sa4.sphinx.model.Device;
+import ch.usi.inf.sa4.sphinx.model.Room;
 import ch.usi.inf.sa4.sphinx.model.User;
 
 import java.util.HashMap;
@@ -9,6 +11,8 @@ import java.util.HashMap;
  */
 public class Storage {
     private static HashMap<String, User> users;
+    private static HashMap<Integer, Room> rooms;
+    private static HashMap<Integer, Device> devices;
 
     /**
      * Retrieves a user given its username.
@@ -20,6 +24,24 @@ public class Storage {
     }
 
     /**
+     * Retrieves a room given its id.
+     * @param id the id of the requested room
+     * @return the requested room or null if no room with that id exists.
+     */
+    public static Room getRoom(Integer id) {
+        return rooms.get(id);
+    }
+
+    /**
+     * Retrieves a device given its id.
+     * @param id the id of the requested device
+     * @return the requested device or null if no device with that id exists.
+     */
+    public static Device getDevice(Integer id) {
+        return devices.get(id);
+    }
+
+    /**
      * Inserts a user.
      * @param user the user to insert
      */
@@ -28,11 +50,43 @@ public class Storage {
     }
 
     /**
+     * Inserts a room.
+     * @param room the room to insert
+     */
+    public static void insertRoom(Room room) {
+        rooms.put(room.id, roon);
+    }
+
+    /**
+     * Inserts a device.
+     * @param device the device to insert
+     */
+    public static void insertDevice(Device device) {
+        devices.put(device.id, device);
+    }
+
+    /**
      * Deletes the user with the given username. Has no effect if no such user exists.
      * @param username the username of the user to delete
      */
     public static void deleteUser(String username) {
         users.remove(username);
+    }
+
+    /**
+     * Deletes the room with the given id. Has no effect if no such room exists.
+     * @param id the id of the room to delete
+     */
+    public static void deleteRoom(Integer id) {
+        rooms.remove(id);
+    }
+
+    /**
+     * Deletes the device with the given id. Has no effect if no such device exists.
+     * @param id the id of the device to delete
+     */
+    public static void deleteDevice(Integer id) {
+        devices.remove(id);
     }
 
     /**
