b740e6e114fa60f4c8a32e68c3ab1fd020849be8
davide.trupia <trupid@usi.ch>
2020-03-09 17:23:58 +0100

    Added Room Class

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Room.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Room.java
@@ -1,5 +1,56 @@
 package ch.usi.inf.sa4.sphinx.model;
+import java.util.ArrayList;
 
 public class Room {
+	protected String name;
+	protected String background;
+	protected String icon;
+	protected ArrayList<Integer> devices;
+	// protected int id;
 
+	public Room(String name){
+		this.name=name;
+		background = "/images/default_room";
+		icon = "/images/default_icon";
+		devices = new ArrayList<>();
+	}
+
+	public Room(){
+		name= "Room"; // + id (?)
+		background = "/images/default_room";
+		icon = "/images/default_icon";
+		devices = new ArrayList<>();
+	}
+
+	//-------- getter and setter for name ----------------------
+	public String getName(){
+		return this.name;
+	}
+
+	public void setName(String newName){
+		this.name = newName;
+	}
+
+	//-------- getter and setter for background -----------------
+	public String getBackground(){
+		return this.background;
+	}
+
+	public void setBackground(String newBg){
+		this.background = newBg;
+	}
+
+	//--------- getter and setter for icon ----------------------
+	public String getIcon(){
+		return this.icon;
+	}
+
+	public void setIcon(String newIcon){
+		this.icon = newIcon;
+	}
+
+	//---------- getter for devices ----------------
+	public ArrayList<Integer> getDevices(){
+		return devices;
+	}
 }
