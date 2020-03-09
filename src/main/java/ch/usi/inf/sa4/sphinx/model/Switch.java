1cd42563249ec0e832aa3814a5ccca58db433c7d
canzom <canzoniere70@gmail.com>
2020-03-09 13:48:53 +0100

    RoomController class created

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Switch.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Switch.java
@@ -2,12 +2,18 @@ package ch.usi.inf.sa4.sphinx.model;
 
 public class Switch extends Device {
     private int state;
+
+    /* Returns the power percentage for a given switch.
+     * @return the value (percentage) of a given switch
+     */
     public int getPower(){
         return state;
     }
-
+    /* Computes whether the power is 'on' or 'off'.
+     * @return a String stating whether switch is 'on' or 'off'
+     */
     public String getLabel(){
-        return state == 0 ? "FALSE": "ON";
+        return state == 0 ? "OFF": "ON";
     }
 
 }
