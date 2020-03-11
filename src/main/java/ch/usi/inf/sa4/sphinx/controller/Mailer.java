a2eacbae09b61e5b6137042d79265dc4da57160c
terehm <terehm@usi.ch>
2020-03-11 13:12:55 +0100

    write the maybe hopefully final mailer

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/Mailer.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/Mailer.java
@@ -1,13 +1,26 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
-public class Mailer implements EmailService {
-    @Autowired
+@Service
+public class Mailer {
+
     public JavaMailSender mailSender;
 
     public void send(String to, String subject, String message) {
 
+        SimpleMailMessage mail = new SimpleMailMessage();
+
+        mail.setTo(to);
+        mail.setSubject(subject);
+        mail.setText(message);
+
+        // TODO: maybe put this into a configuration file somewhere somehow?
+        mail.setFrom("info@smarthut.xyz");
+
+        mailSender.send(mail);
     }
+
 }
