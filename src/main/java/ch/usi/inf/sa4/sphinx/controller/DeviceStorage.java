d4f8fc4ee60f78f1d293e1e28f5a53d1c1664ed8
Tripparsugo <federico.lagrasta@gmail.com>
2020-03-20 21:37:45 +0100

    saving changes

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/DeviceStorage.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/DeviceStorage.java
@@ -1,4 +1,38 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
-public class DeviceStorage {
+import ch.usi.inf.sa4.sphinx.model.Device;
+
+public interface DeviceStorage {
+    /**
+     * Retrieves a Device given its id.
+     *
+     * @param roomId the id of the requested room
+     * @return the requested room or null if no room with that id exists.
+     */
+    Device get(final String roomId);
+
+
+    /**
+     * Inserts a copy of the given Device into storage.
+     *
+     * @param device the room to insert
+     * @return a copy of the Device stored or null if the operation fails
+     */
+    Device insert(final Device device);
+
+
+    /**
+     * Deletes the Device with the given Id. Has no effect if no such Device exists.
+     *
+     * @param deviceId the id of the Device to delete
+     */
+    void delete(String deviceId);
+
+
+    /**
+     * Updates the Room found with the id in updatedRoom with the fields of updatedRoom
+     * @param updatedDevice the user with the updated field
+     * @return true if the operation is successful else false
+     */
+    boolean updateUser(final Device updatedDevice);
 }
--- src/main/java/ch/usi/inf/sa4/sphinx/controller/RoomStorage.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/RoomStorage.java
@@ -1,6 +1,40 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
-public class RoomStorage {
+import ch.usi.inf.sa4.sphinx.model.User;
+import ch.usi.inf.sa4.sphinx.model.Room;
 
+public interface RoomStorage {
+    /**
+     * Retrieves a room given its id.
+     *
+     * @param roomId the id of the requested room
+     * @return the requested room or null if no room with that id exists.
+     */
+    Room get(final String roomId);
+
+
+    /**
+     * Inserts a copy of the given room into storage.
+     *
+     * @param room the room to insert
+     * @return a copy of the Room stored or null if the operation fails
+     */
+    Room insert(final Room room);
+
+
+    /**
+     * Deletes the room with the given Id. Has no effect if no such room exists.
+     *
+     * @param roomId the id of the room to delete
+     */
+    public void delete(String roomId);
+
+
+    /**
+     * Updates the Room found with the id in updatedRoom with the fields of updatedRoom
+     * @param updatedRoom the user with the updated field
+     * @return true if the operation is successful else false
+     */
+    boolean update(final Room updatedRoom);
 
 }
