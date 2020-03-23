3e9d4296b0ed873d0095e5ab931b741fffe5bdbb
ryakhm <ryakhm@usi.ch>
2020-03-23 07:17:55 +0100

    waiting for instructions

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/RoomController.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/RoomController.java
@@ -1,4 +1,14 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
+import ch.usi.inf.sa4.sphinx.model.Room;
+import ch.usi.inf.sa4.sphinx.view.SerialisableDevice;
+import ch.usi.inf.sa4.sphinx.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rooms")
 public class RoomController {
+
+
 }
