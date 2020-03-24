2ce7df42d646dab44abe8dbcb8fa1d7978f8c186
Tripparsugo <federico.lagrasta@gmail.com>
2020-03-24 12:37:45 +0100

    changed DeviceController to work with the services

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/DeviceController.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/DeviceController.java
@@ -1,34 +1,56 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
 
+import ch.usi.inf.sa4.sphinx.misc.DeviceType;
 import ch.usi.inf.sa4.sphinx.model.Device;
 import ch.usi.inf.sa4.sphinx.model.User;
+import ch.usi.inf.sa4.sphinx.service.DeviceService;
+import ch.usi.inf.sa4.sphinx.service.RoomService;
+import ch.usi.inf.sa4.sphinx.service.UserService;
 import ch.usi.inf.sa4.sphinx.view.SerialisableDevice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/devices")
 public class DeviceController {
+
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    DeviceService deviceService;
+    @Autowired
+    RoomService roomService;
 
     /**
      * @param sessionToken session token of the user
      * @return a ResponseEntity with the ids of the devices owned by the user
      */
     @GetMapping("/")
-    public ResponseEntity<int[]> getUserDevices(@RequestHeader("session-token") String sessionToken, @RequestHeader("user") String username) {
-        User user = Storage.getUser(username);
+    public ResponseEntity<Integer[]> getUserDevices(@NotNull @RequestHeader("session-token") String sessionToken,
+                                                    @NotNull @RequestHeader("user") String username,
+                                                    Errors errors) {
 
+
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(401).build();
+        }
+
+        User user = Storage.getUser(username);
         if (user != null) {
-            if (user.getSessionToken() != sessionToken) {
+            if (!user.getSessionToken().equals(sessionToken)) {
                 return ResponseEntity.status(401).build();
             }
 
-            List<Integer> devices = user.getDevices();
-            return ResponseEntity.ok(devices.toArray());
+            List<Integer> devices = userService.getDevices(username);
+            return ResponseEntity.ok(devices.stream().toArray(Integer[]::new));
         }
 
         return ResponseEntity.notFound().build();
@@ -40,59 +62,48 @@ public class DeviceController {
      * @return a ResponseEntity with the data of the requested device (200), not found (404) if no such device exist
      */
     @GetMapping("/{deviceId}")
-    public ResponseEntity<SerialisableDevice> getDevice(@PathVariable Integer deviceId, @RequestHeader("session-token") String sessionToken, @RequestHeader("user") String username) {
-        Device device = Storage.getDevice(deviceId);
+    public ResponseEntity<SerialisableDevice> getDevice(@PathVariable Integer deviceId,
+                                                        @RequestHeader("session-token") String sessionToken,
+                                                        @RequestHeader("user") String username) {
+        deviceService.get(deviceId);
         User user = Storage.getUser(username);
 
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        if (user.getSessionToken() != sessionToken) {
+        if (!userService.validSession(username, sessionToken) || userService.ownsDevice(username, deviceId)) {
             return ResponseEntity.status(401).build();
         }
 
-        if (device != null) {
-            return ResponseEntity.ok(new SerialisableDevice(device, user));
-        }
+        return ResponseEntity.ok(new SerialisableDevice(deviceService.get(deviceId), user));
 
 
     }
 
 
     /**
-     * @param device data of the device to be created, name, label and icon are required
+     * @param device       data of the device to be created, name, label and icon are required
+     * @param sessionToken sessionToken of the use
+     * @param username     username of the user
+     * @param errors       errors in validating the fields
      * @return a ResponseEntity with the data of the newly created device (203), or
      * 500 in case of a server error
      */
     @PostMapping("/")
-    public ResponseEntity<SerialisableDevice> createDevice(@RequestBody SerialisableDevice device, @RequestHeader("session-token") String sessionToken, @RequestHeader("user") String username) {
-        User user = Storage.getUser(username);
+    public ResponseEntity<SerialisableDevice> createDevice(@NotNull @RequestBody SerialisableDevice device,
+                                                           @NotBlank @RequestHeader("session-token") String sessionToken,
+                                                           @NotBlank @RequestHeader("user") String username,
+                                                           Errors errors) {
 
-
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        if (user.getSessionToken() != sessionToken) {
-            return ResponseEntity.status(401).build();
-        }
-
-
-        if (device.name == null || device.label == null || device.icon == null) {
+        if (errors.hasErrors()) {
             return ResponseEntity.badRequest().build();
         }
 
+        if (!userService.validSession(username, sessionToken)) {
+            return ResponseEntity.status(401).build();
+        }
 
-        //TODO differend kind of devices depending on the label, might benefit from a factory
-        Device newDevice = null;
+        User user = userService.get(username);
+        Integer deviceId = roomService.addDevice(device.roomId, DeviceType.intToDeviceType(device.type));
 
-
-        //TODO make storage make the ids?
-        newDevice.setId(UUID.randomUUID().toString());
-        Storage.insertDevice(newDevice);
-
-        return ResponseEntity.status(201).body(new SerialisableDevice(newDevice, user));
+        return ResponseEntity.status(201).body(new SerialisableDevice(deviceService.get(deviceId), user));
 
     }
 
@@ -105,33 +116,33 @@ public class DeviceController {
      * 500 in case of a server error
      */
     @PutMapping("/{deviceId}")
-    public ResponseEntity<Device> modifyDevice(@PathVariable Integer deviceId, @RequestBody String name, @RequestBody String icon) {
-        if (user == null) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<SerialisableDevice> modifyDevice(@NotBlank @PathVariable Integer deviceId,
+                                                           @NotBlank @RequestBody String name,
+                                                           @NotBlank @RequestBody String icon,
+                                                           @NotBlank @RequestHeader("session-token") String sessionToken,
+                                                           @NotBlank @RequestHeader("user") String username,
+                                                           Errors errors) {
+
+
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
         }
 
-        if (user.getSessionToken() != sessionToken) {
+        if (!userService.validSession(username, sessionToken) || !userService.ownsDevice(username, deviceId)) {
             return ResponseEntity.status(401).build();
         }
 
+        User user = userService.get(username);
+        Device device = deviceService.get(deviceId);
+        device.setIcon(icon);
+        device.setName(name);
 
-        var devices = user.getDevices();
-        for (Device device : devices) {
-            if (device.getId() == deviceId) {
-                if (name != null) {
-                    device.setName(name);
-                }
-                if (icon != null) {
-                    device.setIcon(icon);
-                }
 
-                Storage.insertDevice(device);
-
-                return ResponseEntity.status(203).build();
-            }
+        if (deviceService.update(device)) {
+            return ResponseEntity.status(201).body(new SerialisableDevice(deviceService.get(deviceId), user));
         }
 
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(500).build();
 
     }
 
@@ -140,31 +151,21 @@ public class DeviceController {
      * @return a ResponseEntity
      */
     @DeleteMapping("/{deviceId}")
-    public ResponseEntity<Device> deleteDevice(@PathVariable Integer deviceId, @RequestHeader("session-token") String sessionToken, @RequestHeader("user") String username) {
+    public ResponseEntity<Device> deleteDevice(@NotBlank @PathVariable Integer deviceId,
+                                               @RequestHeader("session-token") String sessionToken,
+                                               @NotBlank @RequestHeader("user") String username,
+                                               Errors errors) {
 
-        User user = Storage.getUser(username);
-
-
-        if (user == null) {
-            return ResponseEntity.notFound().build();
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().build();
         }
 
-        if (user.getSessionToken() != sessionToken) {
+        if (!userService.ownsDevice(username, deviceId) || !userService.validSession(username, sessionToken)) {
             return ResponseEntity.status(401).build();
         }
+        userService.removeDevice(username, deviceId);
 
-
-        var devices = user.getDevices();
-        for (Device device : devices) {
-            if (device.getId() == deviceId) {
-                Storage.deleteDevice(deviceId);
-                return ResponseEntity.status(202).build();
-            }
-        }
-
-
-        return ResponseEntity.notFound().build();
-
+        return ResponseEntity.status(202).build();
     }
 
 
--- src/main/java/ch/usi/inf/sa4/sphinx/model/Room.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Room.java
@@ -74,5 +74,6 @@ public class Room {
 	
 	public void removeDevice(Integer device){
 		devices.remove(device);
+
 	}
 }
