c6cc7a8025deb91fea2e50f6526e44a80094942f
davide.trupia <trupid@usi.ch>
2020-03-27 14:59:26 +0100

    #61: Model Test set-up

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Device.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Device.java
@@ -109,9 +109,4 @@ public abstract class Device extends Storable<Integer, Device> {
             effect.execute(event.get());
         }
     }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> #61: added tests for Device, Dimmable and child classes. Improved room test.
 }
