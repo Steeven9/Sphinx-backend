2897b77685fce2fac40996b2aeb7dab62556a8a3
ryakhm <ryakhm@usi.ch>
2020-03-28 19:54:30 +0100

    #64: Add tests for DeviceType

---
+++ src/test/java/ch/usi/inf/sa4/sphinx/view/SerialisableDeviceTest.java
@@ -0,0 +1,10 @@
+package ch.usi.inf.sa4.sphinx.view;
+
+import ch.usi.inf.sa4.sphinx.model.Device;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SerialisableDeviceTest {
+    SerialisableDevice serialisableDevice;
+    Device device;
+}
