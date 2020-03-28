4a0d95b6da8c7da2b9c1b4502b3fd79ac3c55a15
ryakhm <ryakhm@usi.ch>
2020-03-28 19:54:30 +0100

    #64: Add tests for DeviceType

---
+++ src/test/java/ch/usi/inf/sa4/sphinx/misc/SerialiserTest.java
@@ -0,0 +1,35 @@
+package ch.usi.inf.sa4.sphinx.misc;
+
+import ch.usi.inf.sa4.sphinx.model.Device;
+import ch.usi.inf.sa4.sphinx.model.Light;
+import ch.usi.inf.sa4.sphinx.model.Room;
+import ch.usi.inf.sa4.sphinx.model.User;
+import ch.usi.inf.sa4.sphinx.view.SerialisableDevice;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SerialiserTest {
+
+    @Autowired
+    private static Serialiser serialiser;
+
+
+    SerialisableDevice serialisableDevice;
+    Device device;
+    User user;
+    Room room;
+
+    @Test
+    void isSerializedDevice() {
+        serialisableDevice = new SerialisableDevice();
+        device = new Light();
+        var x = serialiser.serialiseDevice(device);
+        var z = x.getClass();
+        var y = serialisableDevice.getClass();
+        assertEquals(z, y);
+    }
+}
