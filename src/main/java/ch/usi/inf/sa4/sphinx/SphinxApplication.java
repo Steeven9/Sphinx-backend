33872a96005a5eac0bc33bd99f9dd7f137055f62
Tripparsugo <federico.lagrasta@gmail.com>
2020-03-29 19:29:25 +0200

    add demo.properties file, property injection to be fixed

--- src/main/java/ch/usi/inf/sa4/sphinx/SphinxApplication.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/SphinxApplication.java
@@ -1,16 +1,17 @@
 package ch.usi.inf.sa4.sphinx;
 
 import ch.usi.inf.sa4.sphinx.Demo.DummyDataAdder;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
-@PropertySource("file:")
+@PropertySource("classpath:/demo.properties")
 public class SphinxApplication {
+
 
 	@Value("${dummy_enabled:true}")
 	private static boolean dummyDataEnabled;
@@ -18,7 +19,8 @@ public class SphinxApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SphinxApplication.class, args);
-		if (true) {
+		if (dummyDataEnabled) {
+			System.out.println("HERE");
 			DummyDataAdder dda = new DummyDataAdder();
 			dda.dummy1();
 		}
