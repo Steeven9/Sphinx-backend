f652875673b283e052df86fcebc08fb86b3e3cd7
davide.trupia <trupid@usi.ch>
2020-03-29 18:02:24 +0200

    #61: added tests for Device, Dimmable and child classes. Improved room test.

--- src/test/java/ch/usi/inf/sa4/sphinx/model/RoomTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/RoomTest.java
@@ -1,9 +1,6 @@
 package ch.usi.inf.sa4.sphinx.model;
 
-<<<<<<< HEAD
 import ch.usi.inf.sa4.sphinx.view.SerialisableDevice;
-=======
->>>>>>> #61: Models RoomTest, DeviceTest and CouplingTest done
 import ch.usi.inf.sa4.sphinx.view.SerialisableRoom;
 import org.junit.jupiter.api.Test;
 
@@ -15,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RoomTest {
     Room r = new Room();
-<<<<<<< HEAD
 
     @Test
     void shouldReturnTrueIfTheTwoCopiesAreEqual() {
@@ -116,108 +112,6 @@ class RoomTest {
     }
 
     @Test
-=======
-
-    @Test
-    void shouldReturnTrueIfTheTwoCopiesAreEqual() {
-        Room newRoom = r.makeCopy();
-//      assertTrue(r.equals(newRoom));
-    }
-
-    @Test
-    void shouldSetARoomUsingTheConstructorSerialisable(){
-        SerialisableRoom serialisableRoom = new SerialisableRoom(100, "test_name", "test_icon", "test_bg", new Integer[]{1, 2, 3, 4});
-        Room room = new Room(serialisableRoom);
-//        assertTrue(serialisableRoom.equals(room));
-    }
-
-    @Test
-    void shouldReturnFalseOnSetIdToNull() {
-        assertTrue(r.setId(null));
-    }
-
-    @Test
-    void shouldReturnTrueOnSetIdToInteger() {
-        assertTrue(r.setId(1));
-    }
-
-    @Test
-    void shouldReturnTrueOnSetIdWithIdAlreadyExisting() {
-        r.setId(2);
-        assertFalse(r.setId(1));
-    }
-
-    @Test
-    void shouldReturnNullWithIdEqualToNull() {
-        r.setId(null);
-        assertEquals(null, r.getId());
-    }
-
-    @Test
-    void shouldReturnIdNumber() {
-        r.setId(3);
-        assertEquals(3, r.getId());
-    }
-
-    @Test
-    void shouldReturnNameOnSettingNameToString() {
-        r.setName("TEST_NAME");
-        assertEquals("TEST_NAME", r.getName());
-    }
-
-    @Test
-    void shouldReturnNullOnSettingNameToNull() {
-        r.setName(null);
-        assertEquals(null, r.getName());
-    }
-
-    @Test
-    void shouldReturnBackgroundOnSettingNameToString() {
-        r.setBackground("TEST_BG");
-        assertEquals("TEST_BG", r.getBackground());
-    }
-
-    @Test
-    void shouldReturnNullOnSettingBackgroundToNull() {
-        r.setBackground(null);
-        assertEquals(null, r.getBackground());
-    }
-
-    @Test
-    void shouldReturnIconOnSettingIconToString() {
-        r.setIcon("TEST_ICON");
-        assertEquals("TEST_ICON", r.getIcon());
-    }
-
-    @Test
-    void shouldReturnNullOnSettingIconToNull() {
-        r.setIcon(null);
-        assertEquals(null, r.getIcon());
-    }
-
-    @Test
-        void shouldReturnListOnSettingDevicesToIntegerList() {
-
-            int test_device1 = 1;
-            r.addDevice(test_device1);
-
-            int test_device2 = 2;
-            r.addDevice(test_device2);
-
-            List<Integer> test_devices = Arrays.asList(test_device1,test_device2);
-            assertEquals(test_devices, r.getDevices());
-    }
-
-    @Test
-    void shouldReturnNullOnSettingDevicesToNull() {
-        List<Integer> test_devices = Arrays.asList(null, null);
-        r.addDevice(null);
-        r.addDevice(null);
-        assertEquals(test_devices, r.getDevices());
-    }
-
-    @Test
->>>>>>> #61: Models RoomTest, DeviceTest and CouplingTest done
     void shouldReturnDevicesWithTheDeviceRemoved() {
         int test_device1 = 1;
         int test_device2 = 2;
@@ -235,7 +129,6 @@ class RoomTest {
         r.addDevice(test_device6);
         r.addDevice(test_device7);
         r.removeDevice(4);
-<<<<<<< HEAD
         assertEquals(test_devices, r.getDevices());
     }
 
@@ -247,13 +140,6 @@ class RoomTest {
         SerialisableRoom sd = d.serialise();
         assertEquals("TEST_NAME", sd.name);
         assertEquals(23, sd.id);
-=======
-        assertEquals(test_devices,r.getDevices());
     }
 
-    @Test
-    void serialise() {
-
->>>>>>> #61: Models RoomTest, DeviceTest and CouplingTest done
-    }
 }
