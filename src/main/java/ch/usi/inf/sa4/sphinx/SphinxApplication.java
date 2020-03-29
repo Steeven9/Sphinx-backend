f2d1b162ae71cee132e23a3830f6296176fade6c
Tripparsugo <federico.lagrasta@gmail.com>
2020-03-29 17:44:58 +0200

    #59 add new dummy data

--- src/main/java/ch/usi/inf/sa4/sphinx/SphinxApplication.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/SphinxApplication.java
@@ -1,19 +1,27 @@
 package ch.usi.inf.sa4.sphinx;
 
 import ch.usi.inf.sa4.sphinx.Demo.DummyDataAdder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource("file:")
 public class SphinxApplication {
+
+	@Value("${dummy_enabled:true}")
+	private static boolean dummyDataEnabled;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SphinxApplication.class, args);
-		DummyDataAdder dda =  new DummyDataAdder();
-		dda.dummy1();
+		if (true) {
+			DummyDataAdder dda = new DummyDataAdder();
+			dda.dummy1();
+		}
 	}
 
 }
