18f543ff81645e1403f0fbe77749c3c95c697d36
davide.trupia <trupid@usi.ch>
2020-04-05 13:16:47 +0200

    #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved

--- src/test/java/ch/usi/inf/sa4/sphinx/model/LightTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/LightTest.java
@@ -3,6 +3,8 @@ package ch.usi.inf.sa4.sphinx.model;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LightTest {
@@ -11,11 +13,16 @@ class LightTest {
     void shouldReturnCopyIfMakeCopy() {
         Light l = new Light();
         l.setName("TEST_LIGHT");
+<<<<<<< HEAD
+=======
+        l.couplings = new ArrayList<>();
+>>>>>>> #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved
 
         Device l2 = l.makeCopy();
         assertNotNull(l2);
         assertEquals(l.getId(), l2.getId());
         assertEquals(l.getName(), l2.getName());
+<<<<<<< HEAD
     }
 
     @Test
@@ -32,4 +39,26 @@ class LightTest {
         assertEquals("off",l.getLabel());
     }
 
+=======
+    }
+
+    @Test
+    void shouldReturnOnLabelIfCalledGetLabel() {
+        Light l = new Light();
+        l.setOn(true);
+        l.couplings = new ArrayList<>();
+
+        assertEquals("on",l.getLabel());
+    }
+
+    @Test
+    void shouldReturnOffLabelIfCalledGetLabel() {
+        Light l = new Light();
+        l.setOn(false);
+        l.couplings = new ArrayList<>();
+
+        assertEquals("off",l.getLabel());
+    }
+
+>>>>>>> #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved
 }
