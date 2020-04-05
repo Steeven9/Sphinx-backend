becc14641cb3fb0dd166c0482f95650cc39c5d42
davide.trupia <trupid@usi.ch>
2020-04-05 13:16:47 +0200

    #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved

--- src/test/java/ch/usi/inf/sa4/sphinx/model/EffectTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/EffectTest.java
@@ -1,9 +1,9 @@
 package ch.usi.inf.sa4.sphinx.model;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class EffectTest {
-    @Disabled
     @Test
     void shouldReturnTrueIfSettingIdIsSetBefore() {
         DimmableLightStateSet d = new DimmableLightStateSet(9);
