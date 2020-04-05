0f6ad212cad7677980368f9656439daad3e5faf4
davide.trupia <trupid@usi.ch>
2020-04-05 13:16:47 +0200

    #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved

--- src/test/java/ch/usi/inf/sa4/sphinx/model/SmartPlugTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/SmartPlugTest.java
@@ -1,9 +1,9 @@
 package ch.usi.inf.sa4.sphinx.model;
+
 import ch.usi.inf.sa4.sphinx.view.SerialisableDevice;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-
 
 import static org.junit.jupiter.api.Assertions.*;
 
