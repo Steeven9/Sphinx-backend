877c0d59068a071ccd27c95abfd7b3855045561c
davide.trupia <trupid@usi.ch>
2020-04-05 13:16:47 +0200

    #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved

--- src/test/java/ch/usi/inf/sa4/sphinx/model/LightSensorTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/LightSensorTest.java
@@ -1,13 +1,57 @@
 package ch.usi.inf.sa4.sphinx.model;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import java.text.*;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LightSensorTest {
 
+    @Disabled
     @Test
-    void makeCopy() {
+    void shouldReturnCorrectQuantityIfCallGetQuantity() {
+        Sensor l = new LightSensor();
+        String s = l.getLabel();
+        DecimalFormat df = new DecimalFormat("#.#");
+        int comma = 0;
+        String s2 = "";
+        int c = 0;
+
+        for(c = 0;comma <=0;c++){
+            s2 += s.charAt(c);
+            if(s.charAt(c) == '.'){
+                comma++;
+            }
+        }
+        s2+=s.charAt(c);
+
+        Random random = new Random();
+        double rd = random.nextDouble();
+        double comparing = 500 + (rd - 0.5);
+        String comparing_cutted = df.format(comparing);
+
+        int comma2 = 0;
+        String comparing_new = "";
+        int i = 0;
+        for(i = 0;comma2 <=0;c++){
+            if(comparing_cutted.charAt(i) == ','){
+                comparing_new += ".";
+                comma2++;
+            }else{
+                comparing_new += s.charAt(i);
+            }
+        }
+        comparing_new +=s.charAt(i);
+
+        while(!s2.equals(comparing_cutted)){
+            rd = random.nextDouble();
+            comparing = 500.0 + (rd -0.5);
+            comparing_cutted = df.format(comparing);
+        }
+        assertEquals(comparing_cutted + "" + l.getPhQuantity(), s2);
     }
 
     @Test
--- src/test/java/ch/usi/inf/sa4/sphinx/model/LightTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/LightTest.java
@@ -2,15 +2,40 @@ package ch.usi.inf.sa4.sphinx.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LightTest {
 
     @Test
-    void makeCopy() {
+    void shouldReturnCopyIfMakeCopy() {
+        Light l = new Light();
+        l.setName("TEST_LIGHT");
+        l.couplings = new ArrayList<>();
+
+        Device l2 = l.makeCopy();
+        assertNotNull(l2);
+        assertEquals(l.getId(), l2.getId());
+        assertEquals(l.getName(), l2.getName());
     }
 
     @Test
-    void getLabel() {
+    void shouldReturnOnLabelIfCalledGetLabel() {
+        Light l = new Light();
+        l.setOn(true);
+        l.couplings = new ArrayList<>();
+
+        assertEquals("on",l.getLabel());
     }
+
+    @Test
+    void shouldReturnOffLabelIfCalledGetLabel() {
+        Light l = new Light();
+        l.setOn(false);
+        l.couplings = new ArrayList<>();
+
+        assertEquals("off",l.getLabel());
+    }
+
 }
