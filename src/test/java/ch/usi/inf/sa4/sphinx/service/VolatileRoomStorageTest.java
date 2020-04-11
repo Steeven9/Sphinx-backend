f7372c2039f5d1b9bd5a0de04d99f4ef207e08bf
ryakhm <ryakhm@usi.ch>
2020-04-11 20:49:43 +0200

    #65: create all tests for VolatileRoomStorage

--- src/test/java/ch/usi/inf/sa4/sphinx/service/VolatileRoomStorageTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/service/VolatileRoomStorageTest.java
@@ -1,22 +1,13 @@
 package ch.usi.inf.sa4.sphinx.service;
 
 import ch.usi.inf.sa4.sphinx.model.Room;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 import java.util.Map;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -25,23 +16,10 @@ class VolatileRoomStorageTest {
     @Autowired
     VolatileRoomStorage roomStorage;
 
-    static Stream<Arguments> argumentsProvider() {
-        Room room1 = new Room();
-        Room room2 = new Room();
-        Room room3 = new Room();
-        room1.setName("testRoom1");
-        room3.setName("testRoom3");
-        room1.setBackground("testBackground1");
-        room3.setBackground("testBackground3");
-        return Stream.of(
-                arguments(room1, room1.getName(), room1.getBackground()),
-                arguments(room2, room1.getName(), room1.getBackground()),
-                arguments(room3, room1.getName(), room1.getBackground())
-        );
-    }
 
     @Test
     @Order(1)
+    @DisplayName("Test that roomStorage is initialized")
     void testInitializer() {
         Map<Integer, Room> data = roomStorage.data;
         assertEquals(0, data.size());
@@ -49,6 +27,7 @@ class VolatileRoomStorageTest {
 
     @Test
     @Order(2)
+    @DisplayName("Test correct functionality of generateKey method")
     void testGenerateKey() {
         Integer id = roomStorage.generateKey(new Room());
         assertEquals(1, id);
@@ -58,20 +37,52 @@ class VolatileRoomStorageTest {
 
     @Test
     @Order(3)
-    void testStorageFunctionality_InsertingOneNewRoom() {
+    void testStorageFunctionality_InsertingAndDeleting() {
         Integer id = 3;
-        assertNull(roomStorage.get(id));
         Room room1 = new Room();
         room1.setName("testRoom3");
         room1.setBackground("testBackground3");
-        Integer id1 = roomStorage.insert(room1);
-        assertEquals(id, id1);
+
+        assertNull(roomStorage.get(id));
+        Integer id3 = roomStorage.insert(room1);
+        assertEquals(id, id3);
+        assertNotEquals(room1, roomStorage.get(id3));//does not points to same object
+
         Room returnedRoom = roomStorage.get(id);
         assertNotNull(returnedRoom);
         assertAll(
                 () -> assertEquals("testBackground3", returnedRoom.getBackground()),
                 () -> assertEquals("testRoom3", returnedRoom.getName()),
-                () -> assertEquals(3, roomStorage.data.size())
+                () -> assertEquals(1, roomStorage.data.size())
         );
+
+        roomStorage.delete(id3);
+        assertNull(roomStorage.get(id3));
+
+        Room roomWithLockedKey = new Room();
+        roomWithLockedKey.lockKey();
+        assertNull(roomStorage.insert(roomWithLockedKey));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test correct functionality of update method")
+    void testUpdate() {
+        Room roomWithoutKey = new Room();
+        Room roomWithNotExestingKey = new Room();
+        roomWithNotExestingKey.setKey(222);
+        assertFalse(roomStorage.update(roomWithoutKey));
+        assertFalse(roomStorage.update(roomWithNotExestingKey));
+
+        Room room = new Room();
+
+        Integer id = roomStorage.insert(room);
+        assertEquals(room.getName(), roomStorage.get(id).getName());
+
+        String testName = "testNameForUpdate";
+        room.setName(testName);
+
+        assertTrue(roomStorage.update(room));
+        assertEquals(testName, roomStorage.get(id).getName());
     }
 }
