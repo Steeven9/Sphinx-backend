936f9145011749f5adc4f22aff1500439122096d
davide.trupia <trupid@usi.ch>
2020-04-14 15:53:24 +0200

    Deleted empty tests for Models as requested

--- src/test/java/ch/usi/inf/sa4/sphinx/model/EffectTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/EffectTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class EffectTest {
-    @Disabled
     @Test
     void shouldReturnFalseIfSettingIdIsSetBefore() {
         Device d = new Light();
-        d.setId(1);
-        assertFalse(d.setId(4283));
+        assertEquals(d.setId(4283));
     }
 
 }
