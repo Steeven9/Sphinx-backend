c29612a664cc124f393adbe31797d1fdf682c56c
davide.trupia <trupid@usi.ch>
2020-04-14 18:56:37 +0200

    #61: conflicts resolution

--- src/test/java/ch/usi/inf/sa4/sphinx/model/DimmableTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/DimmableTest.java
@@ -1,8 +1,4 @@
 package ch.usi.inf.sa4.sphinx.model;
-<<<<<<< HEAD
-=======
-
->>>>>>> #61: added tests for Device, Dimmable and child classes. Improved room test.
 import ch.usi.inf.sa4.sphinx.view.SerialisableDevice;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -25,8 +21,6 @@ class DimmableTest {
         assertEquals("TEST_NAME", sd.name);
         assertEquals(23, sd.id);
         assertFalse(d.isOn());
-<<<<<<< HEAD
-=======
     }
 
     @Disabled
@@ -34,30 +28,6 @@ class DimmableTest {
     void shouldReturnTheStateAfterSettingItMultipleElements() {
         Dimmable d = new Dimmable();
         d.couplings.add(11);
-        d.setState(0.4);
-        assertEquals(0.4, d.getState());
-    }
-
-    @Test
-    void shouldReturnTheStateAfterSettingIt() {
-        Dimmable d = new Dimmable();
-        d.setState(0.4);
-        assertEquals(0.4, d.getState());
->>>>>>> #61: added tests for Device, Dimmable and child classes. Improved room test.
-    }
-
-    @Disabled
-    @Test
-<<<<<<< HEAD
-    void shouldReturnTheStateAfterSettingItMultipleElements() {
-        Dimmable d = new Dimmable();
-        d.couplings.add(11);
-=======
-    void shouldThrowExceptionIfTheStateIsSetWrong() {
-        Dimmable d = new Dimmable();
-        Dimmable d2 = new Dimmable(d);
-        assertThrows(IllegalArgumentException.class,()->d2.setState(1.1));
->>>>>>> #61: added tests for Device, Dimmable and child classes. Improved room test.
     }
         @Test
         void shouldReturnTheStateAfterSettingIt () {
@@ -86,21 +56,4 @@ class DimmableTest {
             assertEquals(d.couplings, d2.couplings);
         }
 
-<<<<<<< HEAD
-=======
-    @Test
-    void shouldReturnLabelCorrectly() {
-        Dimmable d = new Dimmable();
-        assertEquals("100.0%", d.getLabel());
-    }
-
-    @Test
-    void shouldCopyDimmableCorrectly() {
-        Dimmable d = new Dimmable();
-        Dimmable d2 = d.makeCopy();
-        assertEquals(d.couplings, d2.couplings);
-    }
-
-
->>>>>>> #61: added tests for Device, Dimmable and child classes. Improved room test.
 }
--- src/test/java/ch/usi/inf/sa4/sphinx/model/LightTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/model/LightTest.java
@@ -13,16 +13,11 @@ class LightTest {
     void shouldReturnCopyIfMakeCopy() {
         Light l = new Light();
         l.setName("TEST_LIGHT");
-<<<<<<< HEAD
-=======
-        l.couplings = new ArrayList<>();
->>>>>>> #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved
 
         Device l2 = l.makeCopy();
         assertNotNull(l2);
         assertEquals(l.getId(), l2.getId());
         assertEquals(l.getName(), l2.getName());
-<<<<<<< HEAD
     }
 
     @Test
@@ -39,26 +34,4 @@ class LightTest {
         assertEquals("off",l.getLabel());
     }
 
-=======
-    }
-
-    @Test
-    void shouldReturnOnLabelIfCalledGetLabel() {
-        Light l = new Light();
-        l.setOn(true);
-        l.couplings = new ArrayList<>();
-
-        assertEquals("on",l.getLabel());
-    }
-
-    @Test
-    void shouldReturnOffLabelIfCalledGetLabel() {
-        Light l = new Light();
-        l.setOn(false);
-        l.couplings = new ArrayList<>();
-
-        assertEquals("off",l.getLabel());
-    }
-
->>>>>>> #61: added tests for Effect, LightSensor and Light, SmartPlug and Devices improved
 }
