e5cf8cb6e6ed77b68853bd17326231a2333d1e0f
Tripparsugo <federico.lagrasta@gmail.com>
2020-04-18 08:10:52 +0200

    #90 refactor userService pt1

--- src/main/java/ch/usi/inf/sa4/sphinx/service/VolatileEffectStorage.java
+++
@@ -1,9 +0,0 @@
-package ch.usi.inf.sa4.sphinx.service;
-
-import ch.usi.inf.sa4.sphinx.model.Effect;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public final class VolatileEffectStorage extends VolatileIntegerKeyStorage<Effect<?>> {
-    private VolatileEffectStorage() {}
-}
