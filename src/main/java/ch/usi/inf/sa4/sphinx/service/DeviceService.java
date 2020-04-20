ac902ff9eb3695c346a2e5085814820ced0adc01
ryakhm <ryakhm@usi.ch>
2020-04-20 14:43:40 +0200

    #51: create addCoupling method in DeviceService

--- src/main/java/ch/usi/inf/sa4/sphinx/service/DeviceService.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/service/DeviceService.java
@@ -1,14 +1,20 @@
 package ch.usi.inf.sa4.sphinx.service;
 
-import ch.usi.inf.sa4.sphinx.model.Device;
+import ch.usi.inf.sa4.sphinx.misc.DeviceType;
+import ch.usi.inf.sa4.sphinx.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @Service
 public final class DeviceService {
 
+    @Autowired
+    CouplingService couplingService;
     @Autowired
     Storage<Integer, Device> deviceStorage;
 
@@ -16,7 +22,7 @@ public final class DeviceService {
      * @param deviceId the Id of the device
      * @return the Device with corresponding deviceId
      */
-    public Device get(Integer deviceId){
+    public Device get(Integer deviceId) {
         return deviceStorage.get(deviceId);
     }
 
@@ -25,7 +31,85 @@ public final class DeviceService {
      * @param device the device to update
      * @return true if the update succeds else false
      */
-    public boolean update(Device device){
+    public boolean update(Device device) {
         return deviceStorage.update(device);
+    }
+
+    //todo fix increment in StatelessDimmSwitchChangedEvent(key, 1.0)
+
+    /**
+     * Generates an Event basing on switch type.
+     *
+     * @param type the DeviceType of a switch
+     * @param key  the id of the switch
+     * @return event corresponding to the switch
+     */
+    private Event<?> eventHelper(DeviceType type, Integer key) {
+        if (DeviceType.SWITCH.equals(type)) {
+            return new SwitchChangedEvent(key);
+        } else if (DeviceType.STATELESS_DIMMABLE_SWITCH.equals(type)) {
+            return new StatelessDimmSwitchChangedEvent(key, 1.0);
+        } else if (DeviceType.DIMMABLE_SWITCH.equals(type)) {
+            return new DimmSwitchChangedEvent(key);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Generates an Effect basing on switch type and light type.
+     *
+     * @param switchType the DeviceType of a switch
+     * @param lightType  the DeviceType of a light
+     * @param key        the id of the switch
+     * @return corresponding effect
+     */
+    private <T> Effect<T> effectHelper(DeviceType switchType, DeviceType lightType, Integer key, Event<T> effect) {
+        if (DeviceType.SWITCH.equals(switchType)) {
+            return (Effect<T>) (new DeviceSetOnEffect(key));
+        } else if (DeviceType.DIMMABLE_LIGHT.equals(lightType)) {
+            if (DeviceType.STATELESS_DIMMABLE_SWITCH.equals(switchType)) {
+                return (Effect<T>) (new DimmableLightStateInc(key));
+            } else if (DeviceType.DIMMABLE_SWITCH.equals(switchType)) {
+                return (Effect<T>) (new DimmableLightStateSet(key));
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Creates a coupling between a light and a switch.
+     *
+     * @param device1 first device to couple
+     * @param device2 second device to couple
+     * @return true if coupling was succeed, false otherwise
+     */
+    public <T> boolean createCoupling(Device device1, Device device2) {
+        ArrayList<DeviceType> switches = new ArrayList<>(Arrays.asList(DeviceType.SWITCH, DeviceType.DIMMABLE_SWITCH, DeviceType.STATELESS_DIMMABLE_SWITCH));
+        ArrayList<DeviceType> lights = new ArrayList<>(Arrays.asList(DeviceType.LIGHT, DeviceType.DIMMABLE_LIGHT));
+        DeviceType type1 = DeviceType.deviceToDeviceType(device1);
+        DeviceType type2 = DeviceType.deviceToDeviceType(device2);
+        boolean ordered;
+
+        //check if first is a switch and second is a light
+        if (switches.contains(type1) && lights.contains(type2)) {
+            ordered = true;
+        } else if (switches.contains(type2) && lights.contains(type1)) {
+            ordered = false;
+        } else {
+            return false;
+        }
+
+        if (ordered) {
+            Event<?> event = eventHelper(type1, device1.getKey());
+            couplingService.addCoupling(event, effectHelper(type1, type2, device2.getKey(), event));
+        } else {
+            Event<?> event = eventHelper(type2, device2.getKey());
+            couplingService.addCoupling(event, effectHelper(type2, type1, device1.getKey(), event));
+        }
+        return true;
     }
 }
