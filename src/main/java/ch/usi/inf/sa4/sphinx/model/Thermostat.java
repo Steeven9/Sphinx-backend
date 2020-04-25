96df98176c4a3632c5024139bbbf522fad0c9612
ryakhm <ryakhm@usi.ch>
2020-04-25 15:51:15 +0200

    #70: create Thermostat class and a method in RoomService to compute the AVG temp

---
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Thermostat.java
@@ -0,0 +1,68 @@
+package ch.usi.inf.sa4.sphinx.model;
+
+import ch.usi.inf.sa4.sphinx.service.CouplingService;
+import ch.usi.inf.sa4.sphinx.service.RoomService;
+
+import java.text.DecimalFormat;
+
+/**
+ * A Thermostat is a device, which can control the temperature in a given room. It is embedded to a Temperature sensor.
+ */
+public class Thermostat extends TempSensor {
+    private double targetTemp;
+    private double averageTemp;
+    private int state;
+
+    public Thermostat(RoomService roomService, CouplingService couplingService) {
+        super(roomService, couplingService);
+        this.targetTemp = this.getValue();
+        this.state = 1; //idle
+        this.averageTemp = this.targetTemp;
+    }
+
+    public Thermostat(Thermostat d) {
+        super(d);
+        this.targetTemp = d.targetTemp;
+        this.state = d.state;
+        this.averageTemp = d.averageTemp;
+    }
+
+    public double getAverageTemp() {
+        return averageTemp;
+    }
+
+    public void setAverageTemp(double averageTemp) {
+        this.averageTemp = averageTemp;
+    }
+
+    /**
+     * Sets a target temperature to this thermostat and changes the internal state.
+     *
+     * @param target the target temperature
+     */
+    public void setTargetTemp(double target) {
+        if (target > this.getValue()) {
+            this.state = 3; //heating
+        } else if (target < this.getValue()) {
+            this.state = 2; //cooling
+        }
+        this.targetTemp = target;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getLabel() {
+        return new DecimalFormat("#.##").format(this.getValue()) + " " + this.getPhQuantity() + " Avg: " + this.getAverageTemp();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Thermostat makeCopy() {
+        return new Thermostat(this);
+    }
+}
