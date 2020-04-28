b6f4917ffe2cd2d31e4be916386474c0e3bafaee
canzom <canzoniere70@gmail.com>
2020-04-28 20:17:53 +0200

    #124: Guest controller changes applied

--- src/main/java/ch/usi/inf/sa4/sphinx/controller/GuestController.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/controller/GuestController.java
@@ -45,7 +45,7 @@ public class GuestController {
      */
     @GetMapping("/")
 
-    public ResponseEntity<SerialisableUser[]> getGuests(@RequestHeader("session-token") String session_token, @RequestHeader("username") String username) {
+    public ResponseEntity<SerialisableUser[]> getGuests(@RequestHeader("session_token") String session_token, @RequestHeader("username") String username) {
 
 
         Optional<User> user = userStorage.findByUsername(username);
@@ -64,12 +64,12 @@ public class GuestController {
             return ResponseEntity.ok(users);
         }
 
-        throw new ServerErrorException("");
+        return ResponseEntity.ok(users);
     }
 
 
     /**
-     * Get the list of houses the is allowed to access as guest.
+     * Get the list of houses the user is allowed to access as guest.
      *
      * @param username      the username of the user.
      * @param session_token the session token used for validation
