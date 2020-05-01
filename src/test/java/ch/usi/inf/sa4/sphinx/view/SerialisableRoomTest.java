4ec36ca4222963dadddffe86538265a0cd542214
terehm <terehm@usi.ch>
2020-05-01 10:20:02 +0200

    #136: remove long constructors from Serialisables.
    It is unnecessary. In general Serialisables should only be
    made from their model counterparts anyway.

--- src/test/java/ch/usi/inf/sa4/sphinx/view/SerialisableRoomTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/view/SerialisableRoomTest.java
@@ -1,49 +1,14 @@
 package ch.usi.inf.sa4.sphinx.view;
 
-import ch.usi.inf.sa4.sphinx.model.Room;
-
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SerialisableRoomTest {
-    SerialisableRoom serialisableRoom;
-    Room room;
 
-    static Stream<Arguments> argumentsProvider() {
-        return Stream.of(
-                arguments(101, "testName", "testIcon", "testBackground", new Integer[]{3, 6}),
-                arguments(0, " ", "icon2", "@Back", new Integer[]{3, -6, 0}),
-                arguments(-1, "NONAME", "", "grey", new Integer[]{333333, 0, 6 + 8})
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("argumentsProvider")
-    @DisplayName("Testing constructor with multiple arguments")
-    void testLongConstructor(Integer id, String name, String icon, String background, Integer[] devices) {
-        serialisableRoom = new SerialisableRoom(id, name, icon, background, devices);
-
-
-        assertAll("Should return new SerializedRoom",
-
-                () -> assertEquals(id, serialisableRoom.id),
-                () -> assertEquals(icon, serialisableRoom.icon),
-                () -> assertEquals(background, serialisableRoom.background),
-                () -> assertEquals(devices, serialisableRoom.devices),
-                () -> assertEquals(name, serialisableRoom.name));
-    }
-
+    /* This test is kind of pointless: it is impossible for a constructor to return null.
+    But it will add coverage to the empty constructor, which is enough, since there is nothing else to test. */
     @Test
     @DisplayName("Testing the creation of constructor without parameters")
     void existSerializedRoom() {
