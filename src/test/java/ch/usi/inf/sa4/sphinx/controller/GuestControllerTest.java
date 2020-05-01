fd8f0806efe282b0317b1a23699490f4fe3b826b
canzom <canzoniere70@gmail.com>
2020-05-01 13:57:26 +0200

    #124: Added some tests for guest routes (not implemented)

--- src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
