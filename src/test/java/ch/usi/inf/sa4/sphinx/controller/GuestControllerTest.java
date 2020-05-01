ba4b8eb814ab3a0bb632fbf4f297f1113c16af8d
canzom <canzoniere70@gmail.com>
2020-05-01 12:38:59 +0200

    #124: Gitlab suggestions applied (#3)

--- src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
@@ -1,4 +1,27 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Disabled;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class GuestControllerTest {
+
+    @Autowired
+    private MockMvc mockmvc;
+
 }
