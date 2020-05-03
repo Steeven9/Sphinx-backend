2bb9aaa2efc794932afa13ba2b09936d9e90dedb
Tripparsugo <federico.lagrasta@gmail.com>
2020-05-03 16:05:59 +0200

    #132 fix: inserted user now have a resetCode

--- src/main/java/ch/usi/inf/sa4/sphinx/service/UserService.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/service/UserService.java
@@ -86,6 +86,7 @@ public class UserService {
      */
     public boolean insert(final User user) {
         if (user.getId() != null) return false;
+        user.createResetCode();
 
         userStorage.save(user);
         return true;
