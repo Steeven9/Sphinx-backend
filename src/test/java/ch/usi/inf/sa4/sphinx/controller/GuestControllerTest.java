e90b4a35929afad816f9d0007f0d2f72cb6b9948
canzom <canzoniere70@gmail.com>
2020-05-04 15:17:16 +0200

    no real changes, committing to change branch

--- src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
@@ -85,26 +85,56 @@ public class GuestControllerTest {
                 .andDo(print())
                 .andExpect(status().is(401));
 
+        this.mockmvc.perform(get("/guests/user2/devices/guest2")
+                .header("session-token", "banana")
+                .header("user", "user2"))
+                .andDo(print())
+                .andExpect(status().is(401));
 
 
+        this.mockmvc.perform(delete(("/guests/guest1"))
+                .header("session-token", "banana")
+                .header("user", "user2"))
+                .andDo(print())
+                .andExpect(status().is(401));
 
         this.mockmvc.perform(get("/guests/user2/devices/guest3")
         .header("session-token", "banana")
         .header("user", "user2"))
                 .andDo(print())
                 .andExpect(status().is(401));
-
     }
     @Disabled("Not Implemented")
     @Test
     public void shouldGet401FromWrongUser() throws Exception {
 
         this.mockmvc.perform(get("/guests/")
-
-
                 .header("user", "fakeUser")
                 .header("session-token", "user2SessionToken"))
+                .andDo(print())
+                .andExpect(status().is(401));
 
+        this.mockmvc.perform(get("/guests/houses")
+                .header("user", "fakeUser")
+                .header("session-token", "user2SessionToken"))
+                .andDo(print())
+                .andExpect(status().is(401));
+
+        this.mockmvc.perform(get("/guests/user2/devices/guest1")
+                .header("user", "fakeUser")
+                .header("session-token", "user2SessionToken"))
+                .andDo(print())
+                .andExpect(status().is(401));
+
+//        this.mockmvc.perform(post("/guests/")
+//                .header("user", "fakeUser")
+//                .header("session-token", "user2SessionToken"))
+//                .andDo(print())
+//                .andExpect(status().is(401));
+
+        this.mockmvc.perform(delete(("/guests/guest2"))
+                .header("user", "fakeUser")
+                .header("session-token", "user2SessionToken"))
                 .andDo(print())
                 .andExpect(status().is(401));
 
@@ -206,9 +236,6 @@ public class GuestControllerTest {
 //                .andExpect(status().is(200))
  //               .andExpect(content().contentType(MediaType.APPLICATION_JSON)
 //    }
-
-
-
 
 
 
