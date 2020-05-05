49489d3be3032a86dde7c519e41020fc2e97b788
canzom <canzoniere70@gmail.com>
2020-05-05 17:18:23 +0200

    #124: fixed unseen conflicts in GuestControllerTest

--- src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
+++ src/test/java/ch/usi/inf/sa4/sphinx/controller/GuestControllerTest.java
@@ -1,26 +1,11 @@
 package ch.usi.inf.sa4.sphinx.controller;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> #124: Gitlab suggestions applied (#5)
-=======
->>>>>>> 91b9bfdcfe122435c340422aea538530d8b58595
+
 import ch.usi.inf.sa4.sphinx.Demo.DummyDataAdder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import org.junit.jupiter.api.Test;
->>>>>>> #124: Gitlab suggestions applied (#3)
-=======
->>>>>>> #124: Gitlab suggestions applied (#5)
-=======
->>>>>>> 91b9bfdcfe122435c340422aea538530d8b58595
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,20 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-=======
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
->>>>>>> #124: Gitlab suggestions applied (#3)
-=======
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
->>>>>>> #124: Added some tests for guest routes (not implemented)
-=======
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
->>>>>>> 91b9bfdcfe122435c340422aea538530d8b58595
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,10 +25,6 @@ import org.junit.jupiter.api.Disabled;
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GuestControllerTest {
 
     @Autowired
@@ -269,234 +239,3 @@ public class GuestControllerTest {
 
 }
 
-=======
-=======
->>>>>>> #124: Gitlab suggestions applied (#3)
-=======
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
->>>>>>> #124: Gitlab suggestions applied (#5)
-=======
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
->>>>>>> 91b9bfdcfe122435c340422aea538530d8b58595
-public class GuestControllerTest {
-
-    @Autowired
-    private MockMvc mockmvc;
-
-    @Autowired
-    private DummyDataAdder dummyDataAdder;
-
-
-    @BeforeAll
-    void  init() {
-        dummyDataAdder.addDummyData();
-    }
-
-
-    @Test
-    public void shouldGet400FromNoToken() throws Exception {
-        this.mockmvc.perform(get("/guests/").header("username", "user2"))
-                .andDo(print())
-                .andExpect(status().is(400));
-
-        this.mockmvc.perform(get("/guests/houses").header("username", "user2"))
-                .andDo(print())
-                .andExpect(status().is(400));
-
-        this.mockmvc.perform(get("/guests/user2/devices/guest3").header("username", "user2"))
-                .andDo(print())
-                .andExpect(status().is(400));
-
-        this.mockmvc.perform(post("/guests/").header("username", "user2"))
-                .andDo(print())
-                .andExpect(status().is(400));
-
-        this.mockmvc.perform(delete(("/guests/guest3")).header("username", "user2"))
-                .andDo(print())
-                .andExpect(status().is(400));
-
-//        this.mockmvc.perform(get("/guests/user2/scenes/guest3").header("username", "user2"))
-//                .andDo(print())
-//                .andExpect(status().is(400));
-    }
-
-    @Disabled("Not Implemented")
-    @Test
-    public void shouldGet401FromInvalidToken() throws Exception {
-
-        this.mockmvc.perform(get("/guests/")
-                .header("session-token", "banana")
-                .header("user", "user2"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-        this.mockmvc.perform(get("/guests/houses")
-                .header("session-token", "banana")
-                .header("user", "user2"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-        this.mockmvc.perform(get("/guests/user2/devices/guest2")
-                .header("session-token", "banana")
-                .header("user", "user2"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-
-        this.mockmvc.perform(delete(("/guests/guest1"))
-                .header("session-token", "banana")
-                .header("user", "user2"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-        this.mockmvc.perform(get("/guests/user2/devices/guest3")
-        .header("session-token", "banana")
-        .header("user", "user2"))
-                .andDo(print())
-                .andExpect(status().is(401));
-    }
-    @Disabled("Not Implemented")
-    @Test
-    public void shouldGet401FromWrongUser() throws Exception {
-
-        this.mockmvc.perform(get("/guests/")
-                .header("user", "fakeUser")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-        this.mockmvc.perform(get("/guests/houses")
-                .header("user", "fakeUser")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-        this.mockmvc.perform(get("/guests/user2/devices/guest1")
-                .header("user", "fakeUser")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-//        this.mockmvc.perform(post("/guests/")
-//                .header("user", "fakeUser")
-//                .header("session-token", "user2SessionToken"))
-//                .andDo(print())
-//                .andExpect(status().is(401));
-
-        this.mockmvc.perform(delete(("/guests/guest2"))
-                .header("user", "fakeUser")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-//        this.mockmvc.perform(get("/guests/{username}/devices/{guest_username")
-//        .header("user", "fakeUser")
-//        .header("session-token", "user2SessionToken"))
-//                .andDo(print())
-//                .andExpect(status().is(401));
-    }
-
-
-    @Test
-    public void shouldGet401FromNoGuest() throws Exception {
-
-
-        this.mockmvc.perform(get("/guests/user2/devices/guest10")
-                .header("user", "user2")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-
-
-        this.mockmvc.perform(delete(("/guests/guest23"))
-                .header("user", "user2r")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(401));
-
-//        this.mockmvc.perform(get("/guests/user2/devices/guest59")
-//        .header("user", "fakeUser")
-//        .header("session-token", "user2SessionToken"))
-//                .andDo(print())
-//                .andExpect(status().is(401));
-    }
-
-    @Disabled("401")
-    @Test
-    public void shouldSuccessfullyReturnGuests() throws Exception {
-        this.mockmvc.perform(get("/guests")
-                .header("user", "user2")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-    }
-    @Disabled("401")
-    @Test
-    public void shouldSuccessfullyReturnHousesAccess() throws Exception {
-        this.mockmvc.perform(get("/guests/houses")
-                .header("user", "user2")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-    }
-    @Disabled("401")
-    @Test
-    public void shouldSuccessfullyDeleteGuest() throws Exception {
-        this.mockmvc.perform(delete("/guests/guest2")
-                .header("user", "user2")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(204));
-    }
-    @Disabled("401")
-    @Test
-    public void shouldSuccessfullyCreateGuest() throws Exception {
-
-
-
-        this.mockmvc.perform(post("/guests/")
-                .header("user", "user2")
-                .header("session-token", "user2SessionToken")
-                .content("{\"email\": \"mario@usi.ch\", \"fullname\": \"mariorossi\", \"password\":\"1234\", \"username\": \"user1\"}")
-                .contentType("application/json"))
-                .andDo(print())
-                .andExpect(status().is(204))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-    }
-    @Disabled("401")
-    @Test
-    public void shouldSuccessfullyGetGuestDevices() throws Exception {
-        this.mockmvc.perform(get("/guests/user2/devices/guest1")
-                .header("user", "user2")
-                .header("session-token", "user2SessionToken"))
-                .andDo(print())
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-
-    }
-
-
-//    @Disabled("Not Implemented")
-//    @Test
-//    public void shouldSuccessfullyGetGuestScenes() throws Exception {
-//        this.mockmvc.perform(get("/guests/user2/scenes/{guest_username}").header("user", "user2").header("session-token", "banana"))
-//                .andDo(print())
-//                .andExpect(status().is(200))
- //               .andExpect(content().contentType(MediaType.APPLICATION_JSON)
-//    }
-
-
-
-}
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> #124: Gitlab suggestions applied (#2)
-=======
-
->>>>>>> #124: Gitlab suggestions applied (#5)
-=======
-
->>>>>>> 91b9bfdcfe122435c340422aea538530d8b58595
