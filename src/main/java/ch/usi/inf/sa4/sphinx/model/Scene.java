3a8cc5edbe4da007974becacfc047241ef2f7466
davide.trupia <trupid@usi.ch>
2020-05-21 16:48:43 +0200

    #73: Post /scene route added, added functions to userService and modified some constraints

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Scene.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Scene.java
@@ -1,26 +1,54 @@
 package ch.usi.inf.sa4.sphinx.model;
 
-import ch.usi.inf.sa4.sphinx.view.SerialisableRoom;
 import ch.usi.inf.sa4.sphinx.view.SerialisableScene;
+import com.google.gson.annotations.Expose;
 
+import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Scene{
+/**
+ * Represents a Scene.
+ * The Scene can contain Effect(s)
+ */
+public class Scene implements Runnable{
+    @Expose
     private String name;
+    @Expose
     private String icon;
+    @Expose
+    @OneToMany(orphanRemoval = true,
+            cascade = CascadeType.ALL,
+            mappedBy = "scene",
+            fetch = FetchType.LAZY)
     private List<Effect> effects;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     public Scene(List<Effect> effects, String name, String icon){
         this.effects = effects;
         this.name = name;
         this.icon = icon;
+
+    }
+
+    public Scene(){
+        name = "Scene";
+        effects = new ArrayList<>();
+        icon = "./img/icons/scenes/icon-generic-scene.svg";
+    }
+
+    public Scene(SerialisableScene serialisableScene) {
+        this();
+        if(serialisableScene.name != null) this.name = serialisableScene.name;
+        if(serialisableScene.icon != null) this.icon = serialisableScene.icon;
     }
 
     public String getName(){
         return this.name;
     }
-
     /**
      * Used to set the onwer of the Scene.
      * @param user the owner of this Scene
@@ -55,6 +83,11 @@ public class Scene{
         ss.icon = getIcon();
         ss.name = getName();
         return ss;
+
+    }
+
+    @Override
+    public void run() {
 
     }
 }
