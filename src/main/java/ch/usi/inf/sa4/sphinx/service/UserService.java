f767f3475b5d01233a758828581cbb75210aabe6
Aron.DallePezze <aron.dalle.pezze@usi.ch>
2020-05-23 17:18:54 +0200

    #148: small refactor in method removeGuest of UserService

--- src/main/java/ch/usi/inf/sa4/sphinx/service/UserService.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/service/UserService.java
@@ -344,15 +344,12 @@ public class UserService {
             return false;
         }
 
-        final Optional<User> user = userStorage.findByUsername(host);
-        final Optional<User> guestUser = userStorage.findByUsername(guest);
+        final User user = userStorage.findByUsername(host).orElseThrow(WrongUniverseException::new);
+        final User guestUser = userStorage.findByUsername(guest).orElseThrow(WrongUniverseException::new);
 
-        if (guestUser.isPresent() && user.isPresent()) {
-            guestUser.get().removeHost(user.get());
-            userStorage.save(user.get());
-            return true;
-        }
-        return false;
+        guestUser.removeHost(user);
+        userStorage.save(user);
+        return true;
     }
 
     /**
