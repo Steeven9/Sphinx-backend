97f22113b2ea70684b743d8eebe077ce98e373fd
Tripparsugo <federico.lagrasta@gmail.com>
2020-05-25 13:19:48 +0200

    #74 change: dimmer doens't change slider when turned off

--- src/main/java/ch/usi/inf/sa4/sphinx/model/DimmableSwitch.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/DimmableSwitch.java
@@ -14,4 +14,14 @@ public class DimmableSwitch extends Dimmable {
     public DeviceType getDeviceType() {
         return DeviceType.DIMMABLE_SWITCH;
     }
+
+    @Override
+    public void setOn(boolean on) {
+        if (on != isOn()) {
+            super.setOn(on);
+            triggerEffects();
+        } else {
+            super.setOn(on);
+        }
+    }
 }
