7c0074f7d012e744b5aebb9018a7b658f905e17d
Stefano Taillefert <taills@usi.ch>
2020-05-26 17:43:38 +0200

    #149: This should be a DeviceFactory

--- src/main/java/ch/usi/inf/sa4/sphinx/misc/DeviceType.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/misc/DeviceType.java
@@ -129,44 +129,4 @@ public enum DeviceType {
                 return 0;
         }
     }
-
-
-    /**
-     * Given a DeviceType, returns a new object of that type.
-     *
-     * @param type the device type
-     * @return a new Device according to the DeviceType
-     */
-    public static Device makeDevice(final DeviceType type) {
-        switch (type) {
-            case LIGHT:
-                return new Light();
-            case DIMMABLE_LIGHT:
-                return new DimmableLight();
-            case SWITCH:
-                return new Switch();
-            case DIMMABLE_SWITCH:
-                return new DimmableSwitch();
-            case STATELESS_DIMMABLE_SWITCH:
-                return new StatelessDimmableSwitch();
-            case SMART_PLUG:
-                return new SmartPlug();
-            case HUMIDITY_SENSOR:
-                return new HumiditySensor();
-            case LIGHT_SENSOR:
-                return new LightSensor();
-            case TEMP_SENSOR:
-                return new TempSensor();
-            case MOTION_SENSOR:
-                return new MotionSensor();
-            case SMART_CURTAIN:
-                return new SmartCurtain();
-            case SECURITY_CAMERA:
-                return new SecurityCamera();
-            case THERMOSTAT:
-                return new Thermostat();
-            default:
-                return null;
-        }
-    }
 }
