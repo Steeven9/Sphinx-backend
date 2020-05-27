cc0e030cd070a874fa29724e71befa2d1f1bd175
Tripparsugo <federico.lagrasta@gmail.com>
2020-05-27 20:53:51 +0200

    #74 minor fixes

--- src/main/java/ch/usi/inf/sa4/sphinx/model/Coupling/BadCouplingException.java
+++ src/main/java/ch/usi/inf/sa4/sphinx/model/Coupling/BadCouplingException.java
@@ -2,6 +2,6 @@ package ch.usi.inf.sa4.sphinx.model.Coupling;
 
 public class BadCouplingException extends RuntimeException{
     public BadCouplingException() {
-        super("Copupling impossible between devices");
+        super("Coupling impossible between devices");
     }
 }
